06927e39db7b1e3c2d8974509f14812132fed919
Dylan Thunn <49996239+dthunn@users.noreply.github.com>
2025-08-04 19:10:14 -0600

    blah

--- test/script.py
+++ test/script.py
@@ -5,14 +5,19 @@ from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
 
-## @params: [JOB_NAME]
+# This tells Glue which arguments to expect, JOB_NAME is required
 args = getResolvedOptions(sys.argv, ['JOB_NAME'])
 
 sc = SparkContext()
 glueContext = GlueContext(sc)
 spark = glueContext.spark_session
+
 job = Job(glueContext)
-print("test")
-print("test")
 job.init(args['JOB_NAME'], args)
+
+# Your ETL code starts here
+# For example, a simple print statement
+print("Hello from AWS Glue job!")
+
+# Commit the job to let Glue know it finished successfully
 job.commit()
